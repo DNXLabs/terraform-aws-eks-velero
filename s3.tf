838801edde282cdad44151ab673835596a3ed970
brdelphus <delphus@delphus.org>
2022-05-18 08:52:30 -0300

    Update s3.tf
    acl private is deprecated on new tf versions

--- s3.tf
+++ s3.tf
@@ -2,9 +2,12 @@ resource "aws_s3_bucket" "velero" {
   count = (var.enabled && var.create_bucket) ? 1 : 0
 
   bucket = var.bucket_name
-  acl    = "private"
 
   tags = {
     Name = "EKS Velero"
   }
+}
+resource "aws_s3_bucket_acl" "velero_acl" {
+  bucket = var.bucket_name
+  acl    = "private"
 }
